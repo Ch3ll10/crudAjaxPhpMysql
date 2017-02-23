31e67e19a9e50887535f8791e3845aaa9765cceb
Axel Ulises <ulisesdiazreyes@gmail.com>
2017-02-22 20:26:06 -0600

    Update conexion.php
    delete password LOL and user

--- clases/conexion.php
+++ clases/conexion.php
@@ -3,9 +3,9 @@
   function conectar(){
 	$conn = null;
 	$host = '127.0.0.1';
-	$db	  = 'ejercicios';
-	$user = 'axel';
-	$pwd  = '123456';
+	$db	  = '';
+	$user = '';
+	$pwd  = '';
 
 	/*
 		PDO php de acces object lacual podemos hacer la conexion mas segura los datos contenidos en mysql o cualquier BD
